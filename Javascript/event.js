48cc914ca1f5ab68161f87061aeb6a2484c96f79
Roja Jayashree <rkarne@iwa.mobi>
2018-04-12 16:29:21 -0400

    all chnages

---
+++ Javascript/event.js
@@ -0,0 +1,199 @@
+// go to terminal where these files are sitting and run this command "python -m SimpleHTTPServer 8000" go to http://localhost:8000
+  
+// Array of API discovery doc URLs for APIs used by the quickstart
+  var DISCOVERY_DOCS = ["https://www.googleapis.com/discovery/v1/apis/calendar/v3/rest"];
+
+/** 
+ * Authorization scopes required by the API; multiple scopes can be
+ * included, separated by spaces.
+ * readonly -> var SCOPES = "https://www.googleapis.com/auth/calendar.readonly";
+ * write -> var SCOPES = "https://www.googleapis.com/auth/calendar";
+ */
+  var SCOPES = "https://www.googleapis.com/auth/calendar";
+  var authorizeButton = document.getElementById('authorize-button');
+  var signoutButton = document.getElementById('signout-button');
+  var createdButton = document.getElementById('create-button');
+  var readButton = document.getElementById('read-button');
+  var CLIENT_ID = '449811727072-jjnc5f78s1feh2oreufambdo4l4mdqa3.apps.googleusercontent.com';
+   var API_KEY = 'AIzaSyDx9B3KkpJIcivXEi1Te0sPohbpJJdk0ro'; 
+
+  /**
+   *  On load, called to load the auth2 library and API client library.
+   *
+  function handleClientLoad() {
+    gapi.load('client:auth2', initClient);
+  }*/
+
+  /**
+   * function will operates with client the load of the initialization
+   */
+  function handleClientLoad() {
+    gapi.load('client:auth2', {
+        callback: function(){
+            //handle gapi.Client initialzation
+            // Client ID and API key from the Developer Console
+            
+            initClient(API_KEY, CLIENT_ID);
+        }
+    });
+  }
+
+
+  /**
+   *  Initializes the API client library and sets up sign-in state
+   *  listeners.
+   */
+  function initClient(API_KEY, CLIENT_ID) {
+    gapi.client.init({
+      apiKey: API_KEY,
+      clientId: CLIENT_ID,
+      discoveryDocs: DISCOVERY_DOCS,
+      scope: SCOPES
+    }).then(function () {
+      // Listen for sign-in state changes.
+      gapi.auth2.getAuthInstance().isSignedIn.listen(updateSigninStatus);
+
+      // Handle the initial sign-in state.
+      updateSigninStatus(gapi.auth2.getAuthInstance().isSignedIn.get());
+      authorizeButton.onclick = handleAuthClick;
+      signoutButton.onclick = handleSignoutClick;
+    });
+  }
+
+  /**
+   *  Called when the signed in status changes, to update the UI
+   *  appropriately. After a sign-in, the API is called.
+   */
+  function updateSigninStatus(isSignedIn) {
+    if (isSignedIn) {
+      authorizeButton.style.display = 'none';
+      signoutButton.style.display = 'block';
+      createdButton.style.display = 'block';
+      readButton.style.display = 'block';
+      console.log("calling write event");
+      accessOperation()
+      console.log("calling read event");
+  
+    } else {
+      authorizeButton.style.display = 'block';
+      signoutButton.style.display = 'none';
+      createdButton.style.display = 'none';
+      readButton.style.display = 'none';
+    }
+  }
+
+  /**
+   * Look for the operation requested by the user
+   * create event or read the event list
+   */
+  function accessOperation(){
+      createdButton.onclick = listWriteEvent;
+      readButton.onclick = listUpcomingEvents;
+  }
+
+  /**
+   *  Sign in the user upon button click.
+   */
+  function handleAuthClick(event) {
+    gapi.auth2.getAuthInstance().signIn();
+  }
+
+  /**
+   *  Sign out the user upon button click.
+   */
+  function handleSignoutClick(event) {
+    gapi.auth2.getAuthInstance().signOut();
+  }
+
+  /**
+   * Append a pre element to the body containing the given message
+   * as its text node. Used to display the results of the API call.
+   *
+   * @param {string} message Text to be placed in pre element.
+   */
+  function appendPre(message) {
+    var pre = document.getElementById('content');
+    var textContent = document.createTextNode(message + '\n');
+    pre.appendChild(textContent);
+  }
+
+  /**
+   * Print the summary and start datetime/date of the next ten events in
+   * the authorized user's calendar. If no events are found an
+   * appropriate message is printed.
+   */
+  function listUpcomingEvents() {
+    console.log("called read event");      
+    gapi.client.calendar.events.list({
+      'calendarId': 'primary',
+      'timeMin': (new Date()).toISOString(),
+      'showDeleted': false,
+      'singleEvents': true,
+      'maxResults': 20,
+      'orderBy': 'startTime'
+    }).then(function(response) {
+      var events = response.result.items;
+      appendPre('Upcoming events:');
+      console.log("Length of events:");
+        console.log(events.length);
+      if (events.length > 0) {
+        for (i = 0; i < 20; i++) {
+          var event = events[i];
+          var when = event.start.dateTime;
+          if (!when) {
+            when = event.start.date;
+          }
+          appendPre(event.summary + ' (' + when + ')')
+        }
+      } else {
+        appendPre('No upcoming events found.');
+      }
+    });
+
+  }
+
+   /** Refer to the JavaScript quickstart on how to setup the environment:
+        * https://developers.google.com/calendar/quickstart/js
+        * Change the scope to 'https://www.googleapis.com/auth/calendar' and delete any
+        * stored credentials
+        */
+  function listWriteEvent(){
+    console.log("called write event");      
+        var event = {
+        'summary': 'Google I/O 2018',
+        'location': '800 Howard St., San Francisco, CA 94103',
+        'description': 'A chance to hear more about Google\'s developer products.',
+        'start': {
+        'dateTime': '2018-04-27T09:00:00-07:00',
+        'timeZone': 'America/Los_Angeles'
+        },
+        'end': {
+        'dateTime': '2018-04-27T17:00:00-07:00',
+        'timeZone': 'America/Los_Angeles'
+        },
+        'recurrence': [
+        'RRULE:FREQ=DAILY;COUNT=2'
+        ],
+        'attendees': [
+        {'email': 'lpage@example.com'},
+        {'email': 'sbrin@example.com'}
+        ],
+        'reminders': {
+        'useDefault': false,
+        'overrides': [
+        {'method': 'email', 'minutes': 24 * 60},
+        {'method': 'popup', 'minutes': 10}
+        ]
+        }
+        };
+
+        var request = gapi.client.calendar.events.insert({
+        'calendarId': 'primary',
+        'resource': event
+        });
+
+        request.execute(function(event) {
+        appendPre('Event created: ' + event.htmlLink);
+        //listUpcomingEvents();
+        });
+  }
---
+++ Node/quickstart.js
@@ -0,0 +1,158 @@
+/* ================================================================
+ * v8 by xdf(xudafeng[at]126.com)
+ *
+ * first created at : Tue Jun 30 2015 23:32:56 GMT+0800 (CST)
+ *
+ * ================================================================
+ * Copyright  xdf
+ *
+ * Licensed under the MIT License
+ * You may not use this file except in compliance with the License.
+ *
+ * ================================================================ */
+// Replace this whole code in index.js or main.js and then run node index.js 
+
+'use strict';
+
+module.exports = require('./lib/v8');
+
+const fs = require('fs');
+const mkdirp = require('mkdirp');
+const readline = require('readline');
+const {google} = require('googleapis');
+const OAuth2Client = google.auth.OAuth2;
+const SCOPES = ['https://www.googleapis.com/auth/calendar'];
+const TOKEN_PATH = 'credentials.json';
+
+// Load client secrets from a local file.
+fs.readFile('client_secret.json', (err, content) => {
+  if (err) return console.log('Error loading client secret file:', err);
+  // Authorize a client with credentials, then call the Google Drive API.
+  authorize(JSON.parse(content), listEvents);
+});
+
+/**
+ * Create an OAuth2 client with the given credentials, and then execute the
+ * given callback function.
+ * @param {Object} credentials The authorization client credentials.
+ * @param {function} callback The callback to call with the authorized client.
+ */
+function authorize(credentials, callback) {
+  const {client_secret, client_id, redirect_uris} = credentials.installed;
+  const oAuth2Client = new OAuth2Client(client_id, client_secret, redirect_uris[0]);
+
+  // Check if we have previously stored a token.
+  fs.readFile(TOKEN_PATH, (err, token) => {
+    if (err) return getAccessToken(oAuth2Client, callback);
+    oAuth2Client.setCredentials(JSON.parse(token));
+    callback(oAuth2Client);
+  });
+}
+
+/**
+ * Get and store new token after prompting for user authorization, and then
+ * execute the given callback with the authorized OAuth2 client.
+ * @param {google.auth.OAuth2} oAuth2Client The OAuth2 client to get token for.
+ * @param {getEventsCallback} callback The callback for the authorized client.
+ */
+function getAccessToken(oAuth2Client, callback) {
+  const authUrl = oAuth2Client.generateAuthUrl({
+    access_type: 'offline',
+    scope: SCOPES,
+  });
+  console.log('Authorize this app by visiting this url:', authUrl);
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  });
+  rl.question('Enter the code from that page here: ', (code) => {
+    rl.close();
+    oAuth2Client.getToken(code, (err, token) => {
+      if (err) return callback(err);
+      oAuth2Client.setCredentials(token);
+      // Store the token to disk for later program executions
+      fs.writeFile(TOKEN_PATH, JSON.stringify(token), (err) => {
+        if (err) console.error(err);
+        console.log('Token stored to', TOKEN_PATH);
+      });
+      callback(oAuth2Client);
+    });
+  });
+}
+
+/**
+ * Lists the next 10 events on the user's primary calendar.
+ * @param {google.auth.OAuth2} auth An authorized OAuth2 client.
+ */
+function listEvents(auth) {
+  const calendar = google.calendar({version: 'v3', auth});
+  calendar.events.list({
+    calendarId: 'primary',
+    timeMin: (new Date()).toISOString(),
+    maxResults: 10,
+    singleEvents: true,
+    orderBy: 'startTime',
+  }, (err, {data}) => {
+    if (err) return console.log('The API returned an error: ' + err);
+    const events = data.items;
+    if (events.length) {
+      console.log('Upcoming 10 events:');
+      events.map((event, i) => {
+        const start = event.start.dateTime || event.start.date;
+        console.log(`${start} - ${event.summary}`);
+      });
+    } else {
+      console.log('No upcoming events found.');
+    }
+  });
+// Refer to the Node.js quickstart on how to setup the environment:
+// https://developers.google.com/calendar/quickstart/node
+// Change the scope to 'https://www.googleapis.com/auth/calendar' and delete any
+// stored credentials.
+
+var event = {
+  'summary': 'Google I/O 2015',
+  'location': '800 Howard St., San Francisco, CA 94103',
+  'description': 'A chance to hear more about Google\'s developer products.',
+  'start': {
+    'dateTime': '2015-05-28T09:00:00-07:00',
+    'timeZone': 'America/Los_Angeles',
+  },
+  'end': {
+    'dateTime': '2015-05-28T17:00:00-07:00',
+    'timeZone': 'America/Los_Angeles',
+  },
+  'recurrence': [
+    'RRULE:FREQ=DAILY;COUNT=2'
+  ],
+  'attendees': [
+    {'email': 'lpage@example.com'},
+    {'email': 'sbrin@example.com'},
+  ],
+  'reminders': {
+    'useDefault': false,
+    'overrides': [
+      {'method': 'email', 'minutes': 24 * 60},
+      {'method': 'popup', 'minutes': 10},
+    ],
+  },
+};
+
+calendar.events.insert({
+  auth: auth,
+  calendarId: 'primary',
+  resource: event,
+}, function(err, event) {
+  if (err) {
+    console.log('There was an error contacting the Calendar service: ' + err);
+    return;
+  }
+  console.log('Event created: %s', event.htmlLink);
+});
+
+
+}
+
+
+
+
